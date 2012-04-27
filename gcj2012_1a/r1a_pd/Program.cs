19aba25d1f7374f6ac6d1c3a98c02adecafec83f
Hanetsuki <hanetsuki@gmail.com>
2012-04-28 01:11:08 +0900

    ready for 2012 1a

---
+++ gcj2012_1a/r1a_pd/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace r1a_pd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
