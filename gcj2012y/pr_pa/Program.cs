c541ef99e22d3ac20eeb2ba463363b74e8c98b5c
Hanetsuki <hanetsuki@gmail.com>
2012-05-05 23:58:21 +0900

    Google code jam 2012 Qualification Round

---
+++ gcj2012y/pr_pa/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pr_pa
+{
+    class Env
+    {
+        public StreamReader sr;
+        public StreamWriter sw;
+    }
+
+    class Program
+    {
+
+        static char grepl(char from)
+        {
+            string[,] replacetable = {
+                {" "    ," "},
+                {"a"    ,"y"},
+                {"b"    ,"h"},
+                {"c"    ,"e"},
+                {"d"    ,"s"},
+                {"e"    ,"o"},
+                {"f"    ,"c"},
+                {"g"    ,"v"},
+                {"h"    ,"x"},
+                {"i"    ,"d"},
+                {"j"    ,"u"},
+                {"k"    ,"i"},
+                {"l"    ,"g"},
+                {"m"    ,"l"},
+                {"n"    ,"b"},
+                {"o"    ,"k"},
+                {"p"    ,"r"},
+                {"q"    ,"z"},
+                {"r"    ,"t"},
+                {"s"    ,"n"},
+                {"t"    ,"w"},
+                {"u"    ,"j"},
+                {"v"    ,"p"},
+                {"w"    ,"f"},
+                {"x"    ,"m"},
+                {"y"    ,"a"},
+                {"z"    ,"q"},
+            };
+
+            for (int i = 0; i < replacetable.GetLength(0); i++)
+            {
+                if (from.Equals(replacetable[i,0][0])) {
+                    return replacetable[i,1][0];
+                }
+            }
+            return '*';
+        }
+
+        static void pAcore(long t, Env env)
+        {
+
+            string line_from = env.sr.ReadLine();
+            string line_to = "";
+            int length = line_from.Length;
+            for (int i = 0; i < length; i++) {
+                char char_from = line_from[i];
+                char char_to = grepl(char_from);
+                line_to += char_to;
+                Console.WriteLine("{0}{1}{2}", char_from, char_to, line_to);
+            }
+            env.sw.WriteLine("Case #{0}: {1}", t, line_to);
+
+
+
+/*
+            int k;
+            int[] e;
+            {
+
+                string line = env.sr.ReadLine();
+                string[] parts = line.Split(Env.delim, StringSplitOptions.None);
+                k = Int32.Parse(parts[0]);
+                e = new int[k];
+            }
+            {
+                string line = env.sr.ReadLine();
+                string[] parts = line.Split(Env.delim, StringSplitOptions.None);
+                //Console.WriteLine("line\"{0}\" => {1}", line, parts.Length);
+                for (int i = 0; i < k; i++)
+                {
+                    int ele = Int32.Parse(parts[i]);
+                    e[i] = ele;
+                }
+            }
+            Array.Sort(e);
+            //Console.WriteLine("e{0},e{1},e{2}..", e[0], e[1], e[2]);
+
+            double w = 0;
+            w += ((double)e[0]) * e[1];
+            Console.WriteLine("e{0}:{1},e{2}:{3}..", 0, e[0], 1, e[1]);
+            for (int i = 0; i < (k - 2); i++)
+            {
+                w += ((double)e[i]) * e[i + 2];
+                Console.WriteLine("e{0}:{1},e{2}:{3}..", i, e[i], i + 2, e[i + 2]);
+            }
+            Console.WriteLine("e{0}:{1},e{2}:{3}..", k - 2, e[k - 2], k - 1, e[k - 1]);
+            w += ((double)e[k - 2]) * e[k - 1];
+
+            w *= Math.Sin(Math.PI * 2.0 / k) / 2;
+            Console.WriteLine("Case #{0}: {1}", t, w);
+            env.sw.WriteLine("Case #{0}: {1}", t, w);
+*/
+        }
+
+        static void paLoop(Env env)
+        {
+            string line = env.sr.ReadLine();
+            long t = Int64.Parse(line);
+            for (long i = 1; i <= t; i++)
+            {
+                //Console.WriteLine("case{0}", i);
+                pAcore(i, env);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            StreamReader sr = null;
+            StreamWriter sw = null;
+            Env env = new Env();
+
+            try
+            {
+                sr = new StreamReader(
+                    args[0], Encoding.GetEncoding("Shift_JIS"));
+                env.sr = sr;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                sw = new StreamWriter(
+                    args[1], false, Encoding.GetEncoding("Shift_JIS"));
+                env.sw = sw;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            paLoop(env);
+
+            sw.Close();
+        }
+    }
+}
---
+++ gcj2012y/pr_pc/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace pr_pc
+{
+    class Env
+    {
+        static public string[] delim = { " ", "\t" };
+        public StreamReader sr;
+        public StreamWriter sw;
+    }
+
+    class Program
+    {
+
+        static uint pcCoreSub(uint n, uint a, uint b, uint keta, Dictionary<uint, uint> dict)
+        {
+            uint rv;
+
+            if (dict.ContainsKey(n))
+            {
+                rv = dict[n] + 1u;
+            }
+            else
+            {
+                rv = 0u;
+            }
+
+            uint m = n;
+            uint msdfact = (uint)System.Math.Pow(10, keta - 1);
+
+            for (uint i = 0; i < keta; i++)
+            {
+                m = (m / 10) + (m % 10) * msdfact;
+                if ((n <= m) && (m <= b))
+                dict[m] = rv;
+            }
+
+            return rv;
+        }
+
+        static uint msdKeta(uint n)
+        {
+            uint r = 0;
+            while (n > 0) {
+                r++;
+                n = n / 10;
+            }
+            return r;
+        }
+
+        static void pcCore(long t, Env env)
+        {
+            uint a;
+            uint b;
+            {
+                string line = env.sr.ReadLine();
+                string[] parts = line.Split(Env.delim, StringSplitOptions.None);
+                a = UInt32.Parse(parts[0]);
+                b = UInt32.Parse(parts[1]);
+            }
+
+            uint res = 0;
+            Dictionary<uint, uint> dict = new Dictionary<uint, uint>();
+            uint keta = msdKeta(b);
+
+            for (uint i = a; i <= b; i++)
+            {
+                res = res + pcCoreSub(i, a, b, keta, dict);
+            }
+            env.sw.WriteLine("Case #{0}: {1}", t, res);
+        }
+
+        static void pcLoop(Env env)
+        {
+            string line = env.sr.ReadLine();
+            long t = Int64.Parse(line);
+            for (long i = 1; i <= t; i++)
+            {
+                pcCore(i, env);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            StreamReader sr = null;
+            StreamWriter sw = null;
+            Env env = new Env();
+
+            try
+            {
+                sr = new StreamReader(
+                    args[0], Encoding.GetEncoding("Shift_JIS"));
+                env.sr = sr;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                sw = new StreamWriter(
+                    args[1], false, Encoding.GetEncoding("Shift_JIS"));
+                env.sw = sw;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            pcLoop(env);
+
+            sw.Close();
+        }
+    }
+}
